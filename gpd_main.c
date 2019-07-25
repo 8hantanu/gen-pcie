1d9b8dd511f9867c512d7caa799147d53fb6c2b4
Shantanu Mishra <shantanu.mishra@intel.com>
2019-07-25 00:40:49 -0500

    Reorder callbacks

--- gpd_main.c
+++ gpd_main.c
@@ -2,17 +2,16 @@
 
 MODULE_LICENSE("GPL");
 
-static const char gpd_name[] = "GenPCIeDriv";
-
+static const char gpd_name[] = "GenericPCIeDriver";
 struct pci_dev *pci_dev;
 struct msix_entry *entries;
 unsigned long mmio_addr;
 unsigned long reg_len;
 unsigned long *base_addr;
 
-int device_probe(struct pci_dev *dev, const struct pci_device_id *id);
-int device_init(struct pci_dev *dev);
-void device_remove(struct pci_dev *dev);
+int device_probe(struct pci_dev *pdev, const struct pci_device_id *id);
+int device_init(struct pci_dev *pdev);
+void device_remove(struct pci_dev *pdev);
 
 struct pci_device_id pci_dev_id_gpd[] = {
     {VENDOR_ID, DEVICE_ID, PCI_ANY_ID, PCI_ANY_ID, 0, 0, 0},
@@ -26,45 +25,84 @@ struct pci_driver gen_pcie_driv = {
     .remove = device_remove
 };
 
+
 static int __init gpd_init_module(void) {
     if (pci_register_driver(&gen_pcie_driv))
         GPD_ERR("Driver register failed");
     return 0;
 }
 
+
 static void __exit gpd_exit_module(void) {
     pci_unregister_driver(&gen_pcie_driv);
 }
 
+
 int device_probe(struct pci_dev *pdev, const struct pci_device_id *id) {
     if (pci_enable_device(pdev) != 0 )
         GPD_ERR("Failed while enabling");
+
     pci_dev = pci_get_device(VENDOR_ID, DEVICE_ID, NULL);
-    if (pci_dev)
-        GPD_LOG("Device found with vendor ID %d and devive ID %d", VENDOR_ID, DEVICE_ID);
-    else
+    if (pci_dev) {
+        GPD_LOG("Device found");
+        device_init(pci_dev);
+    } else {
         GPD_ERR("Device not found");
-    device_init(pci_dev);
+    }
+
     return 0;
 }
+
 
 int device_init(struct pci_dev *pdev) {
+
     if (pci_request_regions(pdev, gpd_name))
         GPD_ERR("Region map failed");
+    else
+        GPD_LOG("Mapped memory regions");
+
+    // Enable bus mastering
     pci_set_master(pdev);
-    GPD_LOG("Enabled bus master");
+    GPD_LOG("Enabled bus mastering");
+
+    // Enable PCI INTx for device
     pci_intx(pdev, 0);
-    pci_enable_msix_range(pdev, entries, 1, pci_msix_vec_count(pdev));
-    GPD_LOG("Enabled interrupts");
-	pci_enable_pcie_error_reporting(pdev);
+
+    // Enable MSI-X vectors
+    if (pci_msix_vec_count(pdev) != pci_enable_msix_range(pdev, entries, 1, pci_msix_vec_count(pdev)))
+        GPD_ERR("MSI-X vectors enable failed");
+    else
+        GPD_LOG("Enabled MSI-X vectors");
+
+    // UEMsk
+    u32 mask;
+    // Request address for Advanced Error Reporting capability
+	int pos = pci_find_ext_capability(pdev, PCI_EXT_CAP_ID_ERR);
+	pci_read_config_dword(pdev, pos + PCI_ERR_UNCOR_MASK, &mask);
+	mask |= PCI_ERR_UNC_UNSUP;
+	pci_write_config_dword(pdev, pos + PCI_ERR_UNCOR_MASK, mask);
+
+    // Enable AER capability
+	if (pci_enable_pcie_error_reporting(pdev))
+        GPD_ERR("AER enable failed");
+    else
+        GPD_LOG("Enabled AER");
+
     // not recomm
     pci_set_power_state(pdev, PCI_D3hot);
+
     return 0;
 }
 
+
 void device_remove(struct pci_dev *pdev) {
+
+    pci_disable_pcie_error_reporting(pdev);
+    GPD_LOG("Disabled AER");
+
     pci_release_regions(pdev);
     GPD_LOG("Released mapped regions");
+
     pci_disable_device(pdev);
     GPD_LOG("Disabled device");
 }
