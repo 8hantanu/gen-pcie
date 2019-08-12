1eecea8b293d32dfee9530babdce7deddee33202
Shantanu Mishra <shantanu.mishra@intel.com>
2019-08-12 00:26:36 -0500

    Update device config

--- gpd_config.h
+++ gpd_config.h
@@ -1,8 +1,13 @@
 #ifndef GPD_CONFIG_H
 #define GPD_CONFIG_H
 
-#define VENDOR_ID       0x8086
-#define DEVICE_ID       0x270b
-#define NUM_VFS         4
+#define VENDOR_ID                   0x8086
+#define DEVICE_ID                   0x270b
+#define MAX_NUM_VFS                 16
+#define MAX_NUM_DOMAINS             32
+
+#define NUM_FUNCS_PER_DEV           (MAX_NUM_VFS + 1)
+#define NUM_DEV_FILES_PER_DEVICE    (MAX_NUM_DOMAINS + 1)
+#define MAX_NUM_DEV_FILES           (NUM_FUNCS_PER_DEV * NUM_DEV_FILES_PER_DEVICE)
 
 #endif
